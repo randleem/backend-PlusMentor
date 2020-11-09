88b9e6b1b31993dccb141ad49b3adc5910025e3c
michaelfswann <mikeswann@mail.com>
2020-11-09 17:14:17 +0000

    POST req working from react

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//app.use(require("body-parser").json());
 
 app.use("/", indexRouter);
 
