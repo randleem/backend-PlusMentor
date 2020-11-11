7138bd034a0aaf98ad96de685caecae34f77ea9e
michaelfswann <mikeswann@mail.com>
2020-11-11 16:23:14 +0000

    cookie

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
-const indexRouter = require("./routes/indexRouter");
+const cookieRouter = require("./routes/cookieRouter");
 const interactionRouter = require("./routes/interactionRouter");
 const mentorTeamRouter = require("./routes/mentorTeamRouter");
 const userRouter = require("./routes/userRouter");
-
 
 const app = express();
 
@@ -15,12 +14,11 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("1234"));
 
-app.use("/", indexRouter);
+app.use("/", cookieRouter);
 app.use("/interaction", interactionRouter);
 app.use("/mentor-team", mentorTeamRouter);
 app.use("/user", userRouter);
-
 
 module.exports = app;
